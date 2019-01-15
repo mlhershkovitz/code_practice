a3e28b2694f6dd5ac11e9b482cc52f107953477f
Madison Hershkovitz <mlhersh6@gmail.com>
2019-01-14 18:01:40 -0600

    new folders added

--- jquery practice/assignment/scripts/client.js
+++ jquery practice/assignment/scripts/client.js
@@ -6,8 +6,8 @@ function readyNow() {
 
 }
 
-class Car{
-  constructor( year, make, model ){
+class Car {
+  constructor(year, make, model) {
     this.year = year;
     this.make = make;
     this.model = model;
@@ -15,18 +15,38 @@ class Car{
 } // end Car class
 
 let garage = [];
+let maxCars = 2;
 
-function newCar( year, make, model ){
-  console.log( 'in newCar:', year, make, model );
-  garage.push( new Car( year, make, model ) );
+function newCar(year, make, model) {
+  console.log('in newCar:', year, make, model);
+  garage.push(new Car(year, make, model));
   return true;
-} // end newCar
+}; // end newCar
 
 function addCar() {
-  console.log('add car clicked');
-  let yearIn = $('#yearIn').val();
-  let makeIn = $('#makeIn').val();
-  let modelIn = $ ('#modelIn').val();
-  newCar (yearIn, makeIn, modelIn);
-  $('#carList').append(`<li>${yearIn}, ${makeIn}, ${modelIn}</li>`)
-}
+  if (garage.length < maxCars) {
+    console.log('add car clicked');
+    let yearIn = $('#yearIn').val();
+    let makeIn = $('#makeIn').val();
+    let modelIn = $('#modelIn').val();
+    newCar(yearIn, makeIn, modelIn);
+    $('#carList').append(`<li>${yearIn}, ${makeIn}, ${modelIn}</li>`)
+  } else {
+    console.log('Garage is full');
+    return false;
+  }
+  $('#yearIn').val('');
+  $('#makeIn').val('');
+  $('#modelIn').val('');
+};
+
+// function garageFull(addCar) {
+//   if (garage.length < maxCars) {
+//     addCar();
+//     return true;
+//   }
+//   else {
+//     console.log('Garage is full');
+//     return false;
+//   }
+// };
